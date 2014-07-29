39de74fbd06cd5b5db82307f19c20d214a883abb
larsactionhero <larsactionhero@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-29 20:29:06 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-login-page-logo/trunk@956887 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wp-custom-login-page-logo.js
@@ -0,0 +1,120 @@
+/*
+*	jQuery Scripts for WP Custom Login Page Logo Plugin
+*/
+
+jQuery(document).ready(function($){
+
+	// open thickbox w/ media upload
+	$('.wpclpl-logo-upload-btn').click(function() {
+		tb_show('Select an image file for custom admin logo. (Click "Insert into Post" to select it.)', 'media-upload.php?referer=wpclpl-settings&type=image&TB_iframe=true&post_id=0', false);
+		return false;
+	});
+	
+		
+	// update the css...
+	function updateCustomCss(){
+		var backgroundImage = 'background-image:url('+$('.wpclpl-logo-url').val()+');';
+		var cssPreview = backgroundImage+"\n"+$('.wpclpl-custom-css').val();
+		$('#wpclpl-preview-css').html( cssPreview );
+	}
+		
+	
+	// send data to editor...
+	window.send_to_editor = function(html) {
+		var uploadedLogoUrl = $('img',html).attr('src');
+		$('.wpclpl-logo-url').val(uploadedLogoUrl);
+		tb_remove();
+		$('.wpclpl-currentlogo, .wpclpl-default-logo').fadeOut(300);		
+		$('<img class="wpclpl-logo-preview" style="display:none; "src="'+uploadedLogoUrl+'" />  ')
+			.insertAfter('.wpclpl-currentlogo')
+			.delay(300)
+			.fadeIn(300);
+			
+		$('#wpclpl-logo-preview').attr('src', $('.wpclpl-logo-url').val() );
+		$('#wpclpl-logo-preview-wrap a.thickbox').attr('href', $('.wpclpl-logo-url').val() );
+		
+		updateCustomCss();
+		
+		$('.wpclpl-options-form').delay(600).submit();
+		
+	}
+	
+	
+	function buildPreviewCss(){
+	
+		if( $('.wpclpl-logo-url').val() !="" ){
+			var cssPreview = 'background-image:url("'+$('.wpclpl-logo-url').val()+'");';
+			cssPreview += "\n";
+			cssPreview += $('.wpclpl-custom-css').val();
+			$('#wpclpl-preview-css').html( cssPreview );
+		}
+		
+	}
+	
+	buildPreviewCss();
+	
+	/*
+	// css preview button
+	$('.wpclpl-logo-preview-css-btn').click(function(){
+		var cssPreview = 'background-image:url("'+$('.wpclpl-logo-url').val()+'");';
+		cssPreview += "\n";
+		cssPreview += $('.wpclpl-custom-css').val();
+		$('#wpclpl-preview-css')
+			.html( cssPreview )
+			.slideToggle();
+	});
+	
+	*/
+	
+	// only little bit of example css...
+	/*
+	$('.wpclpl-logo-example-css-btn').click(function(){
+		exampleCss = 'padding: 0px;'+"\n";
+		exampleCss += 'background-size: cover;'+"\n";
+		exampleCss += 'background-position: center center;'+"\n";
+		exampleCss += 'background-repeat: no-repeat;'+"\n";
+		exampleCss += 'background-color: #fff;'+"\n";
+		exampleCss += 'box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);'+"\n";
+		$('.wpclpl-custom-css').val(exampleCss);
+	});
+	*/
+	
+	// remove image and load default
+	$('.wpclpl-logo-remove-img-btn').click(function(){
+		$('.wpclpl-logo-url').val('');
+		$('.wpclpl-options-form').submit();		
+	});
+	
+	
+	// complete reset to default
+	$('.wpclpl-reset-btn').click(function(){
+				
+		$('<div class="wpclpl-modal-box-wrap"></div>').insertBefore('#wpwrap');
+		$('.wpclpl-modal-box').fadeIn(300,function(){
+		
+			// yes, reset...
+			$('.wpclpl-reset-confirmed').click(function(){
+				$('.wpclpl-logo-url, .wpclpl-custom-css, .wpclpl-additional-text').val('');
+				$('.wpclpl-modal-box').fadeOut(300,function(){
+					// note the jQuery bug: .submit() won't work if the submit button	has a "name" tag, 
+					$('.wpclpl-options-form').submit();	
+				});
+			});
+			
+			// no, cancel
+			$('.wpclpl-reset-cancel').click(function(){	
+				$('.wpclpl-modal-box').fadeOut(300);
+			});		
+			
+		});
+			
+	});
+
+
+	
+	// update the preview while typing... just for control	
+	$('.wpclpl-custom-css').keyup(function(){
+		updateCustomCss();
+	});
+	
+});
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== WP Custom Admin Logo ===
+Contributors: Lars Ortlepp
+Donate link: http://wp.larsactionhero.com/development/plugins/wp-custom-login-page-logo/
+Tags: custom admin logo, admin logo, change admin logo, change login logo, admin login logo, custom login page, add client logo to login page, plugin, wp-admin, login logo
+Requires at least: 3.3
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This Plugin lets you customize the Logo of the admin login page panel. Useful if you need to add a client's logo for personalization. 
+
+
+== Description ==
+
+
+
+== Installation ==
+
+1. Download the zip. 
+2. Go to Admin Panel -> Plugins -> Add new.
+3. Upload "wp-custom-admin-logo-1.0.zip" (you can also unzip it and upload it manually to the "/wp-content/plugins/" directory)
+4. Activate the plugin through the 'Plugins' menu in WordPress
+5. The Plugin settings appear at Settings -> Custom Login Page Logo.
+
+== Frequently asked questions ==
+
+Currently there are no FAQ...
+
+= A question that someone might have =
+
+Currently, there's nothing to answer...
+
+== Screenshots ==
+
+http://wp.larsactionhero.com/?p=158
+
+== Changelog ==
+
+= 1.1 =
+* Added custom css edit support
+
+= 1.0 =
+* Initial release
+
+
+
+== Upgrade notice ==
+
+
+
+== Arbitrary section 1 ==
+
